c3e250db8fbd6cd3c5b70c250a1e15b12ed6f84f
maxperea <maxpereaduring@gmail.com>
2018-11-05 19:46:37 +0100

    lol

---
+++ test3/public/index.js
@@ -0,0 +1,88 @@
+const canvas = document.getElementById('spel');
+const context = canvas.getContext('2d');
+
+context.scale(8,8);
+
+var socket;
+socket = io.connect('http://localhost:3000');
+
+var localPlayer = new Character();
+var players = [];
+players.push(localPlayer);
+socket.emit('reqPos', localPlayer);
+socket.emit('charData', localPlayer);
+
+function setBackground(){
+	context.fillStyle = '#202020';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+};
+
+function drawCharacters(chars){
+	for (var i = chars.length - 1; i >= 0; i--) {
+		drawCharacter(chars[i]);
+	}
+}
+
+function updateCharacters(data){
+	for (var i = players.length - 1; i >= 0; i--) {
+		if (players[i].id === data.id) {
+			players[i].pos.x = data.pos.x;
+			players[i].pos.y = data.pos.y;
+			return;
+		}
+	}
+	players.push(data);
+	console.log(data);
+}
+
+function sendPos(){
+	socket.emit('charData', localPlayer);
+}
+
+function draw(){
+	setBackground();
+	drawCharacters(players);
+	socket.on('charData',updateCharacters);
+	socket.on('reqPos', sendPos)
+}
+
+
+function update(){
+	draw();
+	requestAnimationFrame(update);
+}
+
+function drawCharacter(char){
+
+	context.fillStyle = char.color;
+	context.fillRect(char.pos.x, char.pos.y, 5, 5);
+
+}
+
+function Character(){
+	this.id = 0;
+	this.pos = {
+		x: 0,
+		y: 0
+	};
+	this.color = "red";
+}
+
+document.addEventListener('keydown', event => {
+	if (event.keyCode === 37) {
+		localPlayer.pos.x--;
+		socket.emit('charData', localPlayer);
+	} else if (event.keyCode === 39) {
+		localPlayer.pos.x++;
+		socket.emit('charData', localPlayer);
+	} else if (event.keyCode === 40) {
+		localPlayer.pos.y++;
+		socket.emit('charData', localPlayer);
+	} else if (event.keyCode === 38) {
+		localPlayer.pos.y--;
+		socket.emit('charData', localPlayer);
+	}
+});
+
+
+update();
---
+++ test3/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(3000);
+
+var io = socket(server);
+
+app.use(express.static('public'));
+
+
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+	console.log('new connection ' + socket.id);
+
+	socket.on('charData', charMsg);
+	socket.on('reqPos', requestPos);
+
+	function charMsg(data){
+		console.log(data);
+		data.id = socket.id;
+		socket.broadcast.emit('charData', data);
+	};
+
+	function requestPos(data){
+		socket.broadcast.emit('reqPos', data);
+	};
+
+
+}
+
