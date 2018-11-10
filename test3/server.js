afc4bd4505c8ecb715f196733c93a3c677863ffa
maxperea <maxpereaduring@gmail.com>
2018-11-10 22:38:54 +0100

    testar

--- test3/server.js
+++ test3/server.js
@@ -8,32 +8,26 @@ var io = socket(server);
 
 app.use(express.static('public'));
 
-
-
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket){
-	console.log('new connection ' + socket.id);
+  console.log('new connection ' + socket.id);
 
-	socket.on('charData', charMsg);
-	socket.on('reqPos', requestPos);
+  socket.on('charData', charMsg);
+  socket.on('reqPos', requestPos);
 
-	function charMsg(data){
-		console.log(data);
-		data.id = socket.id;
-		socket.broadcast.emit('charData', data);
-	}
+  function charMsg(data){
+    console.log(data);
+    data.id = socket.id;
+    socket.broadcast.emit('charData', data);
+  }
 
-	function requestPos(data){
-		socket.broadcast.emit('reqPos', data);
-	}
+  function requestPos(data){
+    socket.broadcast.emit('reqPos', data);
+  }
 
-	socket.on('disconnect', function(){
-		socket.broadcast.emit('dc');
-		console.log('player discconect');
-	});
-
-
-
+  socket.on('disconnect', function(){
+    socket.broadcast.emit('dc');
+    console.log('player discconect');
+  });
 }
-
